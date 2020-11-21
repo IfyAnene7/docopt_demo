73bd57cd8d7ac297e8a8451e94a7737acc92d8c5
Ify Anene <ifyanene@student.ubc.ca>
2020-11-21 15:50:38 -0500

    Solve exercise 6 by modifying the script further so that at least 3 print statements

--- demo.R
+++ demo.R
@@ -2,16 +2,20 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg4>]        Takes any value (This is optional positional argument)
+[<arg4>]          Takes any value (This is optional positional argument)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
+
+main <- function(var1, var2, var3=NULL, var4=NULL) {
+  print(opt)
+  print(typeof(opt))}
+
+main(opt$arg1, opt$arg2, opt$arg3 = NULL, opt$arg4 = NULL)
